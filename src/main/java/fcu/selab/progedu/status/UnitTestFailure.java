3634f02b182903b9572884e95e9f1a6913f4547a
Moty <zzser15963@gmail.com>
2019-02-18 16:58:59 +0800

    完成UnitTest測試 以及更改<pre> tag

--- src/main/java/fcu/selab/progedu/status/UnitTestFailure.java
+++ src/main/java/fcu/selab/progedu/status/UnitTestFailure.java
@@ -5,24 +5,24 @@ public class UnitTestFailure implements Status {
   public String extractFailureMsg(String consoleText) {
     String tests = "";
     String unitTest = "";
-    String startStr = "-------------------------------------------------------\r\n" + " T E S T S";
-    String goal = "tests run:";
-    int endStr = consoleText.indexOf(goal, consoleText.indexOf(goal) + goal.length());
-    int end = consoleText.indexOf("\n", endStr);
-    tests = consoleText.substring(endStr, end);
+    String startStr = "------------------------------------------------------\n" + " T E S T S";
+    String goal = "Tests run:";
+    int testStr = consoleText.indexOf(goal);
+    int testEnd = consoleText.indexOf("\n", testStr);
+    tests = consoleText.substring(testStr, testEnd);
 
     String errorsStr = "Errors:";
     String failuresStr = "Failures:";
-
     int errorsGoal = tests.indexOf(errorsStr) + errorsStr.length();
     int failuresGoal = tests.indexOf(failuresStr) + failuresStr.length();
-
     String errors = tests.substring(errorsGoal, tests.indexOf(",", errorsGoal)).trim();
     String failures = tests.substring(failuresGoal, tests.indexOf(",", failuresGoal)).trim();
-
     if (errors.equals("0") && failures.equals("0")) {
-      return "";
+      return null;
     }
+
+    // ´M§ä²Ä¤G­ÓTests run:
+    int end = consoleText.indexOf(goal, consoleText.indexOf("\n", testEnd));
     unitTest = consoleText.substring(consoleText.indexOf(startStr), end);
     return unitTest;
   }
