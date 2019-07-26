7a764d002b8fb27260c07c89b6fa5ef05dd1d9f3
Moty <zzser15963@gmail.com>
2019-07-26 15:41:36 +0800

    update all service api

---
+++ front/src/app/views/student/project-choosed/project-choosed.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-project-choosed',
+  templateUrl: './project-choosed.component.html',
+  styleUrls: ['./project-choosed.component.scss']
+})
+export class ProjectChoosedComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ front/src/app/views/student/studashboard/studashboard.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import { environment } from '../../../../environments/environment';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StudashboardService {
+  ALL_COMMIT_API = environment.SERVER_URL + '/ProgEdu/webapi/commits/all';
+  constructor(private http: HttpClient) { }
+
+  getAllStudentData(): Observable<any> {
+    return this.http.get<any>(this.ALL_COMMIT_API);
+  }
+}
