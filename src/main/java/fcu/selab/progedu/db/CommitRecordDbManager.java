26cb0f8bd604b5a4fabd5caf86a3bdc90427908e
angusho53 <yuhoyuho987@gmail.com>
2019-08-09 09:58:50 +0800

    remove unuse function

--- src/main/java/fcu/selab/progedu/db/CommitRecordDbManager.java
+++ src/main/java/fcu/selab/progedu/db/CommitRecordDbManager.java
@@ -140,54 +140,6 @@ public class CommitRecordDbManager {
     }
   }
 
-  // /**
-  // * get Count Group By Hw And Time
-  // *
-  // * @param hw hw number
-  // * @return records
-  // */
-  // public JSONArray getCountGroupByHwAndTime(String hw) {
-  // String status = StatusEnum.INITIALIZATION.getTypeName();
-  // String query = "select date, time, count(status) from Commit_Record where
-  // hw=? and status!="
-  // + "'" + status + "'" + "group by date, time";
-  // JSONArray records = new JSONArray();
-  //
-  // try (Connection conn = database.getConnection();
-  // PreparedStatement preStmt = conn.prepareStatement(query)) {
-  // preStmt.setString(1, hw);
-  // try (ResultSet rs = preStmt.executeQuery();) {
-  // while (rs.next()) {
-  // String date = rs.getString("date");
-  // String time = rs.getString("time");
-  // String[] times = time.split(":");
-  // final double timeValue = Integer.valueOf(times[0]) +
-  // (Integer.valueOf(times[1])) * 0.01;
-  //
-  // Timestamp ts = new Timestamp(System.currentTimeMillis());
-  // ts = Timestamp.valueOf(date + " " + time);
-  //
-  // Timestamp xlabel = new Timestamp(System.currentTimeMillis());
-  // xlabel = Timestamp.valueOf(date + " " + time);
-  // xlabel.setHours(0);
-  // xlabel.setMinutes(0);
-  // xlabel.setSeconds(0);
-  // int count = rs.getInt(COUNT_STATUS);
-  // JSONObject record = new JSONObject();
-  // record.put("x", xlabel.getTime());
-  // record.put("y", timeValue);
-  // record.put("r", count);
-  // record.put("t", ts.getTime());
-  // records.put(record);
-  // }
-  // }
-  //
-  // } catch (SQLException e) {
-  // e.printStackTrace();
-  // }
-  // return records;
-  // }
-
   /**
    * get commit record details from the homework of a student
    * 
