11ceaa33098e724c7104667e9dd8b76b52809945
Moty <zzser15963@gmail.com>
2019-08-20 21:14:43 +0800

    auto add the environment.ts and delete all jsp files

--- front/src/app/views/student/project-choosed/project-choosed.service.ts
+++ front/src/app/views/student/project-choosed/project-choosed.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProjectChoosedService {
-  ALL_PROJECT_API = `ProgEdu/webapi/jenkins/buildDetail?num=1&proName=WEB-HW1&userName=D0350510`;
+  ALL_PROJECT_API = environment.SERVER_URL + `/ProgEdu/webapi/jenkins/buildDetail?num=1&proName=WEB-HW1&userName=D0350510`;
   constructor(private http: HttpClient) { }
 
   getCommitData(): Observable<any> {
--- front/src/environments/environment.prod.ts
+++ front/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  SERVER_URL: 'http://140.134.26.77:8080'
+  SERVER_URL: ''
 };
