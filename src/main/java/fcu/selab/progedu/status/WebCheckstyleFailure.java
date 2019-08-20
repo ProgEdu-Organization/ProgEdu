d9527d0e41db8ebd30c17658b4d8c1b99461c780
angusho53 <yuhoyuho987@gmail.com>
2019-08-20 12:20:46 +0800

    Modify web assignment status

--- src/main/java/fcu/selab/progedu/status/WebCheckstyleFailure.java
+++ src/main/java/fcu/selab/progedu/status/WebCheckstyleFailure.java
@@ -5,11 +5,12 @@ public class WebCheckstyleFailure implements Status {
   @Override
   public String extractFailureMsg(String consoleText) {
     String checkstyleInfo = "WebCheckstyleFailure";
-    String checkstyleStart = "> eslint -c ./src/test/.eslintrc.js ./ & htmlhint ./";
-    String checkstyleEnd = "Scanned";
-    
-    checkstyleInfo = consoleText.substring(consoleText.indexOf(checkstyleStart) 
-        + checkstyleStart.length() + 1, consoleText.lastIndexOf(checkstyleEnd) - 1);
+    String checkstyleStart = "npm run htmlhint";
+    String checkstyleEnd = "Archiving artifacts";
+
+    checkstyleInfo = consoleText.substring(
+        consoleText.indexOf(checkstyleStart) + checkstyleStart.length() + 1,
+        consoleText.lastIndexOf(checkstyleEnd) - 1);
     return checkstyleInfo.trim();
   }
 }
