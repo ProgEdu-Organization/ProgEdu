d1c324294fae4d1f7e3ae59ee5a7b48937ef9a7f
Moty <zzser15963@gmail.com>
2019-08-30 12:56:54 +0800

    update edit assignment

--- front/src/app/views/teacher/assignment-management/assignment-management.service.ts
+++ front/src/app/views/teacher/assignment-management/assignment-management.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from '../../../../environments/environment';
+import { FormGroup } from '@angular/forms';
 
 
 const deleteAssignmentOptions = ({
@@ -23,14 +24,22 @@ export class AssignmentManagementService {
 
   ALL_ASSIGNMENT_API = environment.SERVER_URL + '/ProgEdu/webapi/assignment/getAllAssignments';
   DELETE_ASSIGNMENT_API = environment.SERVER_URL + '/ProgEdu/webapi/assignment/delete';
+  EDIT_ASSIGNMENT_API = environment.SERVER_URL + '/ProgEdu/webapi/assignment/edit';
   constructor(private http: HttpClient) { }
   getAllAssignments(): Observable<any> {
     return this.http.get<any>(this.ALL_ASSIGNMENT_API);
   }
 
-  editAssignment(): Observable<any> {
+  editAssignment(assignment: FormGroup): Observable<any> {
     const form = new FormData();
-    return this.http.post<any>(this.ALL_ASSIGNMENT_API, form, editAssignmentOptions);
+    console.log('test' + assignment.get('name').value);
+    form.append('assignmentName', assignment.get('name').value);
+    form.append('releaseTime', new Date(assignment.get('releaseTime').value).toUTCString());
+    form.append('deadline', new Date(assignment.get('deadline').value).toUTCString());
+    form.append('readMe', assignment.get('readMe').value);
+    form.append('file', assignment.get('file').value);
+
+    return this.http.post<any>(this.EDIT_ASSIGNMENT_API, form, editAssignmentOptions);
   }
 
   deleteAssignment(assignmentName: string): Observable<any> {
@@ -38,4 +47,5 @@ export class AssignmentManagementService {
     form.append('del_Hw_Name', assignmentName);
     return this.http.post<any>(this.DELETE_ASSIGNMENT_API, form, deleteAssignmentOptions);
   }
+
 }
