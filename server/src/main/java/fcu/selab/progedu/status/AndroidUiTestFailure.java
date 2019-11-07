4b6febec407ffcbf32a98aaad1469e379436f4e5
Moty <zzser15963@gmail.com>
2019-11-07 16:37:38 +0800

    edit android status

--- server/src/main/java/fcu/selab/progedu/status/AndroidUiTestFailure.java
+++ server/src/main/java/fcu/selab/progedu/status/AndroidUiTestFailure.java
@@ -4,8 +4,8 @@ public class AndroidUiTestFailure implements Status {
   @Override
   public String extractFailureMsg(String consoleText) {
     String feedback;
-    String feedbackStart = "> Task :app:connectedDebugAndroidTest FAILED";
-    String feedbackEnd = "FAILURE: Build failed with an exception.";
+    String feedbackStart = "> Task :app:connectedDebugAndroidTest";
+    String feedbackEnd = "> Task :app:connectedDebugAndroidTest FAILED";
     feedback = consoleText.substring(consoleText.indexOf(feedbackStart),
             consoleText.indexOf(feedbackEnd));
     return feedback.trim();
