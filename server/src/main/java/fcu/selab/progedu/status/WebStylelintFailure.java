22b2009b854a635d17a6c2fe242fb6bc12e361dd
chenchunwei24k@gmail.com <chenchunwei24k@gmail.com>
2019-11-14 14:46:14 +0800

    delete test case

--- server/src/main/java/fcu/selab/progedu/status/WebStylelintFailure.java
+++ server/src/main/java/fcu/selab/progedu/status/WebStylelintFailure.java
@@ -49,34 +49,4 @@ public class WebStylelintFailure implements Status {
     Gson gson = new Gson();
     return gson.toJson(feedbacklist).toString();
   }
-
-  public String console() {
-    String test = "> node_sample@1.0.0 stylelint /var/jenkins_home/workspace/STUDENT3_1024WEB\n"
-        + "> stylelint ./src/web/\n"
-        + "\n"
-        + "\n"
-        + "src/web/html/index.html\n"
-        + " 7:9   ✖  Expected indentation of 4 spaces                                 "
-        + "indentation                     \n"
-        + " 7:12  ✖  Expected single space after \"{\" of a single-line block           "
-        + "block-opening-brace-space-after \n"
-        + " 7:18  ✖  Expected single space after \":\" with a single-line declaration   "
-        + "declaration-colon-space-after   \n"
-        + " 7:21  ✖  Expected single space before \"}\" of a single-line block          "
-        + "block-closing-brace-space-before\n"
-        + "\n"
-        + "src/web/html/index2.html\n"
-        + " 7:88   ✖  Expected indentation of 4 spaces                                 "
-        + "indentation                     \n"
-        + " 7:92  ✖  Expected single space after \"{\" of a single-line block           "
-        + "block-opening-brace-space-after \n"
-        + " 7:58  ✖  Expected single space after \":\" with a single-line declaration   "
-        + "declaration-colon-space-after   \n"
-        + " 7:21  ✖  Expected single space before \"}\" of a single-line block          "
-        + "block-closing-brace-space-before\n"
-        + "\n"
-        + "npm ERR! code ELIFECYCLE\n"
-        + "npm ERR! errno 2";
-    return test;
-  }
 }
--- server/src/main/java/fcu/selab/progedu/status/WebUnitTestFailure.java
+++ server/src/main/java/fcu/selab/progedu/status/WebUnitTestFailure.java
@@ -52,40 +52,4 @@ public class WebUnitTestFailure implements Status {
     Gson gson = new Gson();
     return gson.toJson(feedbacklist).toString();
   }
-
-  public String console() {
-    String console = "+ npm run test\n"
-        + "> node_sample@1.0.0 test /var/jenkins_home/workspace/test11_1024WEB3\n"
-        + "> mocha ./src/test/*.js --timeout 100000\n"
-        + "\n"
-        + "\n"
-        + "\n"
-        + "  測試index.html\n"
-        + "    ✓ 開啟作業網頁 (788ms)\n"
-        + "    1) 測試input功能\n"
-        + "    ✓ 測試按鈕功能 (77ms)\n"
-        + "    2) 測試頁面title是否為HelloWorld\n"
-        + "\n"
-        + "\n"
-        + "  3 passing (1s)\n"
-        + "  1 failing\n"
-        + "\n"
-        + "  1) 測試index.html\n"
-        + "       測試頁面title是否為HelloWorld:\n"
-        + "\n"
-        + "      AssertionError: expected 'AAAAAAAAAAAAAAAAAAA' to equal 'HelloWorld'\n"
-        + "      + expected - actual\n"
-        + "\n"
-        + "      -AAAAAAAAAAAAAAAAAAA\n"
-        + "      +HelloWorld\n"
-        + "      \n"
-        + "      at Context.it (src/test/indexTest.js:31:22)\n"
-        + "      at process._tickCallback (internal/process/next_tick.js:68:7)\n"
-        + "\n"
-        + "\n"
-        + "\n"
-        + "npm ERR! code ELIFECYCLE";
-    return console;
-  }
-
 }
