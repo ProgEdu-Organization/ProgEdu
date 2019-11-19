4ee14cc6e23cefd83ac6cab840e0d4a0664de6b5
chenchunwei24k@gmail.com <chenchunwei24k@gmail.com>
2019-11-19 13:54:51 +0800

    change JAVA and Maven (include test case)

--- server/src/main/java/fcu/selab/progedu/status/JavacCompileFailure.java
+++ server/src/main/java/fcu/selab/progedu/status/JavacCompileFailure.java
@@ -1,8 +1,13 @@
 package fcu.selab.progedu.status;
 
+import java.io.IOException;
+import java.util.ArrayList;
+
 import fcu.selab.progedu.data.FeedBack;
 
-import java.util.ArrayList;
+import org.codehaus.jackson.JsonGenerationException;
+import org.codehaus.jackson.map.JsonMappingException;
+import org.codehaus.jackson.map.ObjectMapper;
 
 public class JavacCompileFailure implements Status {
 
@@ -19,12 +24,82 @@ public class JavacCompileFailure implements Status {
 
   @Override
   public ArrayList<FeedBack> formatExamineMsg(String consoleText) {
-    return null;
+    int nextRow = consoleText.indexOf("\n");
+    int endIndex = consoleText.length();
+    consoleText = consoleText.substring(nextRow, endIndex);
+    endIndex = endIndex - nextRow - 1;
+    ArrayList<FeedBack> feedbacklist = new ArrayList<>();
+    while (consoleText.indexOf("error:") != -1) {
+      int errorIndex = consoleText.indexOf("error:");
+      nextRow = consoleText.indexOf("\n");
+      if (errorIndex > nextRow) {
+        consoleText = consoleText.substring(nextRow + 1, endIndex);
+        endIndex = endIndex - nextRow - 1;
+      } else {
+        feedbacklist.add(new FeedBack(
+            StatusEnum.COMPILE_FAILURE,
+            consoleText.substring(0, errorIndex)
+                .replace(":", " ").trim(),
+            consoleText.substring(errorIndex + 6, nextRow).trim(),
+            "",
+            ""
+        ));
+        consoleText = consoleText.substring(nextRow + 1, endIndex);
+        endIndex = endIndex - nextRow - 1;
+      }
+    }
+    return feedbacklist;
   }
 
   @Override
   public String toJson(ArrayList<FeedBack> arrayList) {
-    return "Instructor Commit";
+    try {
+      ObjectMapper objectMapper = new ObjectMapper();
+      String jsonString = objectMapper.writerWithDefaultPrettyPrinter()
+          .writeValueAsString(arrayList);
+      return jsonString;
+    } catch (JsonGenerationException e) {
+      e.printStackTrace();
+      return "JsonGenerationException Error";
+    } catch (JsonMappingException e) {
+      e.printStackTrace();
+      return "JsonMappingException Error";
+    } catch (IOException e) {
+      e.printStackTrace();
+      return "IOException Error";
+    }
   }
 
+  public String console() {
+    String test = "using GIT_ASKPASS to set credentials \n"
+        + " > git fetch --tags --progress -- http://140.134.26.62:20100/"
+        + "STUDENT2/JAVA1118.git +refs/heads/*:refs/remotes/origin/*\n"
+        + "skipping resolution of commit remotes/origin/master, since it "
+        + "originates from another repository\n"
+        + " > git rev-parse refs/remotes/origin/master^{commit} # timeout=10\n"
+        + " > git rev-parse refs/remotes/origin/origin/master^{commit} # timeout=10\n"
+        + "Checking out Revision 2fb4ec84ecc73837d38940c88024ed5b1c7c573b"
+        + " (refs/remotes/origin/master)\n"
+        + " > git config core.sparsecheckout # timeout=10\n"
+        + " > git checkout -f 2fb4ec84ecc73837d38940c88024ed5b1c7c573b\n"
+        + "Commit message: \"Update HelloWorld.java\"\n"
+        + " > git rev-list --no-walk 349699222e13fbf9dd7c4a57431efe320e608fa4 # timeout=10\n"
+        + "[STUDENT2_JAVA1118] $ /bin/sh -xe /tmp/jenkins5935095937269347033.sh\n"
+        + "+ javac src/HelloWorld.java\n"
+        + "src/HelloWorld.java:5: error: not a statement\n"
+        + "\t\tcin >> i;\n"
+        + "\t\t    ^\n"
+        + "src/HelloWorld.java:6: error: not a statement\n"
+        + "\t\tcin >> a;\n"
+        + "\t\t    ^\n"
+        + "src/HelloWorld.java:7: error: not a statement\n"
+        + "\t\tcin >> k;\n"
+        + "\t\t    ^\n"
+        + "src/HelloWorld.java:8: error: not a statement\n"
+        + "\t\tcout << j << endl;\n"
+        + "\t\t          ^\n"
+        + "4 errors\n"
+        + "Build step 'Execute shell' marked build as failure";
+    return test;
+  }
 }
--- server/src/main/java/fcu/selab/progedu/status/MavenCheckstyleFailure.java
+++ server/src/main/java/fcu/selab/progedu/status/MavenCheckstyleFailure.java
@@ -1,8 +1,13 @@
 package fcu.selab.progedu.status;
 
+import java.io.IOException;
+import java.util.ArrayList;
+
 import fcu.selab.progedu.data.FeedBack;
 
-import java.util.ArrayList;
+import org.codehaus.jackson.JsonGenerationException;
+import org.codehaus.jackson.map.JsonMappingException;
+import org.codehaus.jackson.map.ObjectMapper;
 
 public class MavenCheckstyleFailure implements Status {
 
@@ -26,11 +31,87 @@ public class MavenCheckstyleFailure implements Status {
 
   @Override
   public ArrayList<FeedBack> formatExamineMsg(String consoleText) {
-    return null;
+    consoleText = consoleText + "\n";
+    int endIndex = consoleText.length();
+    ArrayList<FeedBack> feedbacklist = new ArrayList<>();
+    while (consoleText.indexOf("error:") != -1) {
+      int nextrow = consoleText.indexOf("\n");
+      int nexterror = consoleText.indexOf("error:");
+      if (nexterror > nextrow) {
+        consoleText = consoleText.substring(nextrow + 1, endIndex);
+        endIndex = endIndex - nextrow - 1;
+      } else {
+        String errorRow = consoleText.substring(0, nextrow);
+        int lastslash = errorRow.lastIndexOf("/");
+        feedbacklist.add(new FeedBack(
+            StatusEnum.CHECKSTYLE_FAILURE,
+            errorRow.substring(lastslash + 1, nexterror - 2).trim(),
+            errorRow.substring(nexterror + 6, nextrow).trim(),
+            "",
+            ""
+        ));
+        consoleText = consoleText.substring(nextrow + 1, endIndex);
+        endIndex = endIndex - nextrow - 1;
+      }
+    }
+    return feedbacklist;
   }
 
   @Override
   public String toJson(ArrayList<FeedBack> arrayList) {
-    return "Instructor Commit";
+    try {
+      ObjectMapper objectMapper = new ObjectMapper();
+      String jsonString = objectMapper.writerWithDefaultPrettyPrinter()
+          .writeValueAsString(arrayList);
+      return jsonString;
+    } catch (JsonGenerationException e) {
+      e.printStackTrace();
+      return "JsonGenerationException Error";
+    } catch (JsonMappingException e) {
+      e.printStackTrace();
+      return "JsonMappingException Error";
+    } catch (IOException e) {
+      e.printStackTrace();
+      return "IOException Error";
+    }
+  }
+
+  public String console() {
+    String test = "[INFO] Compiling 1 source file to /var/jenkins_home/"
+        + "workspace/STUDENT2_MAVAN1119/target/classes\n"
+        + "[INFO] \n"
+        + "[INFO] --- maven-checkstyle-plugin:2.17:check (compile) @ HelloMaven ---\n"
+        + "[INFO] Starting audit...\n"
+        + "/var/jenkins_home/workspace/STUDENT2_MAVAN1119/src/main/java/selab"
+        + "/myapp/App.java:8:3: error: Missing a Javadoc comment.\n"
+        + "/var/jenkins_home/workspace/STUDENT2_MAVAN1119/src/main/java/selab"
+        + "/myapp/App.java:10:9: error: Local variable name 'i' must match "
+        + "pattern '^[a-z][a-z0-9][a-zA-Z0-9]*$'.\n"
+        + "/var/jenkins_home/workspace/STUDENT2_MAVAN1119/src/main/java/sela"
+        + "b/myapp/App.java:10:10: error: WhitespaceAround: '=' is not preceded with whitespace.\n"
+        + "/var/jenkins_home/workspace/STUDENT2_MAVAN1119/src/main/java/selab"
+        + "/myapp/App.java:10:11: error: WhitespaceAround: '=' is not followed by whitespace.\n"
+        + "/var/jenkins_home/workspace/STUDENT2_MAVAN1119/src/main/java/sela"
+        + "b/myapp/App.java:11:7: error: WhitespaceAround: 'if' is not followed by whitespace.\n"
+        + "/var/jenkins_home/workspace/STUDENT2_MAVAN1119/src/main/java/sel"
+        + "ab/myapp/App.java:11:9: error: WhitespaceAround: '<' is not preceded with whitespace.\n"
+        + "/var/jenkins_home/workspace/STUDENT2_MAVAN1119/src/main/java/selab/m"
+        + "yapp/App.java:11:10: error: WhitespaceAround: '<' is not followed by whitespace.\n"
+        + "/var/jenkins_home/workspace/STUDENT2_MAVAN1119/src/main/java/selab/m"
+        + "app/App.java:11:12: error: WhitespaceAround: '{' is not preceded with whitespace.\n"
+        + "/var/jenkins_home/workspace/STUDENT2_MAVAN1119/src/main/java/selab/my"
+        + "app/App.java:12: error: 'if' child have incorrect indentation level 8,"
+        + " expected level should be 6.\n"
+        + "/var/jenkins_home/workspace/STUDENT2_MAVAN1119/src/main/java/selab/my"
+        + "app/App.java:12:10: error: WhitespaceAround: '=' is not preceded with whitespace.\n"
+        + "/var/jenkins_home/workspace/STUDENT2_MAVAN1119/src/main/java/selab/m"
+        + "yapp/App.java:12:11: error: WhitespaceAround: '=' is not followed by whitespace.\n"
+        + "/var/jenkins_home/workspace/STUDENT2_MAVAN1119/src/main/java/selab/m"
+        + "yapp/App.java:12:12: error: WhitespaceAround: '+' is not preceded with whitespace.\n"
+        + "/var/jenkins_home/workspace/STUDENT2_MAVAN1119/src/main/java/selab/m"
+        + "yapp/App.java:12:13: error: WhitespaceAround: '+' is not followed by whitespace.\n"
+        + "Audit done.\n"
+        + "[INFO] ------------------------------------------------------------------------";
+    return test;
   }
 }
