18e0fe788b5b075a2f3431538ac883b84fae09c2
Moty <zzser15963@gmail.com>
2019-11-28 16:36:12 +0800

    Fixed checkstyle

--- server/src/main/java/fcu/selab/progedu/service/UserService.java
+++ server/src/main/java/fcu/selab/progedu/service/UserService.java
@@ -13,9 +13,16 @@ import org.glassfish.jersey.media.multipart.FormDataParam;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
-import javax.ws.rs.*;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
