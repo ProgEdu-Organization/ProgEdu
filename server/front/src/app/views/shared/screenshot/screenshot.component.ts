1aba3b88e307b8f5045cc7c722f965ffba540f5c
MomentaryChen <zzser15963@gmail.com>
2019-12-15 18:10:07 +0800

    Update screenshot Title

--- server/front/src/app/views/shared/screenshot/screenshot.component.ts
+++ server/front/src/app/views/shared/screenshot/screenshot.component.ts
@@ -9,6 +9,7 @@ export class ScreenshotComponent implements OnInit, OnChanges {
   @ViewChild('screenshotModal', { static: true }) public screenshotModal: ModalDirective;
   @Input() screenshotUrls;
   @Input() commitNumber;
+  @Input() type;
   @Output() screenshotEvent = new EventEmitter();
 
   selectedScreenshotName: string;
@@ -35,8 +36,17 @@ export class ScreenshotComponent implements OnInit, OnChanges {
 
   rename(url_split: Array<string>) {
     const screenshotName = url_split[url_split.length - 1];
+    let name = '';
     // Change png to html. Ex: index.png -> index.html
-    return screenshotName.substring(0, screenshotName.indexOf('.png')) + '.html';
+    console.log(this.type);
+    if (this.type === 'WEB') {
+      name = screenshotName.substring(0, screenshotName.indexOf('.png')) + '.html';
+    } else if(this.type === 'ANDROID') {
+      name = screenshotName.substring(screenshotName.indexOf('_') + 1
+          , screenshotName.indexOf('.png')) + '.java';
+    }
+    return name;
+
   }
 
 }
