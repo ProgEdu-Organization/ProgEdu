9212b16b9e74180e70b25fe5dc56efbd72662850
chenchunwei <chenchunwei24k@gmail.com>
2020-08-17 18:28:41 +0800

    Add Review Status Enum and modified ReviewStatusDbManager

--- server/src/main/java/fcu/selab/progedu/db/ReviewStatusDbManager.java
+++ server/src/main/java/fcu/selab/progedu/db/ReviewStatusDbManager.java
@@ -5,10 +5,10 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import fcu.selab.progedu.service.ReviewStatusEnum;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import fcu.selab.progedu.service.RoleEnum;
 import fcu.selab.progedu.utils.ExceptionUtil;
 
 public class ReviewStatusDbManager {
@@ -21,4 +21,54 @@ public class ReviewStatusDbManager {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ReviewStatusDbManager.class);
 
+    /**
+     * Get Id by status
+     *
+     * @param status review status
+     * @return id review Id
+     */
+    public int getReviewStatusIdByStatus(String status) {
+        String query = "SELECT id FROM Review_Status WHERE status = ?";
+        int id = 0;
+
+        try (Connection conn = database.getConnection();
+             PreparedStatement preStmt = conn.prepareStatement(query)) {
+            preStmt.setString(1, status);
+            try (ResultSet rs = preStmt.executeQuery();) {
+                while (rs.next()) {
+                    id = rs.getInt("id");
+                }
+            }
+        } catch (SQLException e) {
+            LOGGER.debug(ExceptionUtil.getErrorInfoFromException(e));
+            LOGGER.error(e.getMessage());
+        }
+        return id;
+    }
+
+    /**
+     * Get status by id
+     *
+     * @param id review status Id
+     * @return status review status
+     */
+    public ReviewStatusEnum getReviewStatusById(int id) {
+        String query = "SELECT status FROM Review_Status WHERE id = ?";
+        String reviewStatus = null;
+
+        try (Connection conn = database.getConnection();
+             PreparedStatement preStmt = conn.prepareStatement(query)) {
+            preStmt.setInt(1, id);
+            try (ResultSet rs = preStmt.executeQuery();) {
+                while (rs.next()) {
+                    reviewStatus = rs.getString("status");
+                }
+            }
+        } catch (SQLException e) {
+            LOGGER.debug(ExceptionUtil.getErrorInfoFromException(e));
+            LOGGER.error(e.getMessage());
+        }
+        ReviewStatusEnum reviewStatusEnum = ReviewStatusEnum.getScoreModeEnum(reviewStatus);
+        return reviewStatusEnum;
+    }
 }
