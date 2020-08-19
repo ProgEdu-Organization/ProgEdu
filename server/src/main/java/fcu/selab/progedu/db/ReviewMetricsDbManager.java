14a277528b31c4ed6c6cded99ab3f677421ba71e
chenchunwei <chenchunwei24k@gmail.com>
2020-08-19 13:19:53 +0800

    Add ReviewMetrics class in data and modified ReviewMetricsDbManager

--- server/src/main/java/fcu/selab/progedu/db/ReviewMetricsDbManager.java
+++ server/src/main/java/fcu/selab/progedu/db/ReviewMetricsDbManager.java
@@ -4,11 +4,13 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
+import fcu.selab.progedu.data.ReviewMetrics;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import fcu.selab.progedu.service.RoleEnum;
 import fcu.selab.progedu.utils.ExceptionUtil;
 
 public class ReviewMetricsDbManager {
@@ -20,5 +22,111 @@ public class ReviewMetricsDbManager {
     private IDatabase database = new MySqlDatabase();
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ReviewMetricsDbManager.class);
+
+    /**
+     * Insert review metrics into db
+     *
+     * @param category          category Id
+     * @param mode              mode Id
+     * @param metrics           metrics
+     * @param description       description
+     * @param link              link
+     */
+    public void insertReviewMetrics(int category, int mode, String metrics, String description, String link) {
+        String query = "INSERT INTO Review_Metrics(category, mode, metrics, description, link) "
+         + "VALUES(?,?,?,?,?);";
+
+        try (Connection conn = database.getConnection();
+             PreparedStatement preStmt = conn.prepareStatement(query)) {
+            preStmt.setInt(1, category);
+            preStmt.setInt(2,mode);
+            preStmt.setString(3, metrics);
+            preStmt.setString(4, description);
+            preStmt.setString(5, link);
+            preStmt.executeUpdate();
+        } catch (SQLException e) {
+            LOGGER.debug(ExceptionUtil.getErrorInfoFromException(e));
+            LOGGER.error(e.getMessage());
+        }
+    }
+
+    /**
+     * Get all review metrics by category Id
+     *
+     * @param category          category Id
+     * @return review metrics details
+     */
+    public List<ReviewMetrics> getReviewMetrics(int category) {
+        String query = "SELECT * FROM Review_Metrics WHERE category = ?";
+        List<ReviewMetrics> reviewMetricsList = new ArrayList<>();
+
+        try (Connection conn = database.getConnection();
+             PreparedStatement preStmt = conn.prepareStatement(query)) {
+            preStmt.setInt(1, category);
+            try (ResultSet rs = preStmt.executeQuery()) {
+                while (rs.next()) {
+                    int id = rs.getInt("id");
+                    int mode = rs.getInt("mode");
+                    String metrics = rs.getString("metrics");
+                    String description = rs.getString("description");
+                    String link = rs.getString("link");
+                    ReviewMetrics reviewMetrics = new ReviewMetrics();
+                    reviewMetrics.setId(id);
+                    reviewMetrics.setCategory(category);
+                    reviewMetrics.setMode(mode);
+                    reviewMetrics.setMetrics(metrics);
+                    reviewMetrics.setDescription(description);
+                    reviewMetrics.setLink(link);
+                    reviewMetricsList.add(reviewMetrics);
+                }
+            }
+        } catch (SQLException e) {
+            LOGGER.debug(ExceptionUtil.getErrorInfoFromException(e));
+            LOGGER.error(e.getMessage());
+        }
+        return reviewMetricsList;
+    }
+
+    /**
+     * Update review metrics by Id
+     *
+     * @param id                Id
+     * @param mode              mode Id
+     * @param description       description
+     * @param link              link
+     */
+    public void editReviewMetricsById(int id, int mode, String description, String link) {
+        String query = "UPDATE Review_Metrics SET mode = ?, description = ?, link = ? WHERE id = ?";
+
+        try (Connection conn = database.getConnection();
+             PreparedStatement preStmt = conn.prepareStatement(query)) {
+            preStmt.setInt(1,mode);
+            preStmt.setString(2, description);
+            preStmt.setString(3, link);
+            preStmt.setInt(4, id);
+            preStmt.executeUpdate();
+        } catch (SQLException e) {
+            LOGGER.debug(ExceptionUtil.getErrorInfoFromException(e));
+            LOGGER.error(e.getMessage());
+        }
+    }
+
+    /**
+     * Delete review metrics by Id
+     *
+     * @param id                Id
+     */
+    public void deleteReviewMetrics(int id) {
+        String query = "DELETE FROM Review_Metrics WHERE id = ?";
+
+        try (Connection conn = database.getConnection();
+             PreparedStatement preStmt = conn.prepareStatement(query)) {
+            preStmt.setInt(1, id);
+            preStmt.executeUpdate();
+        } catch (SQLException e) {
+            LOGGER.debug(ExceptionUtil.getErrorInfoFromException(e));
+            LOGGER.error(e.getMessage());
+        }
+    }
 
 }
---
+++ server/src/test/java/fcu/selab/progedu/service/ReviewMetricsDbManagerTest.java
@@ -0,0 +1,60 @@
+package fcu.selab.progedu.service;
+
+import fcu.selab.progedu.data.ReviewMetrics;
+import fcu.selab.progedu.db.ReviewMetricsDbManager;
+import org.junit.Test;
+
+import java.util.List;
+
+public class ReviewMetricsDbManagerTest {
+
+    ReviewMetricsDbManager reviewMetricsDbManager = new ReviewMetricsDbManager();
+
+    @Test
+    public void insertReviewMetricsTest() {
+//        reviewMetricsDbManager.insertReviewMetrics(1, 1,
+//                "符合題目要求",
+//                "這次的提交是否達到開發者的預期目的?也就是學生是否達到老師題目要求",
+//                "https://www.google.com/");
+//        reviewMetricsDbManager.insertReviewMetrics(2, 1,
+//                "排版是否整齊",
+//                "不規則的排版會造成審查人員在閱讀上會有困難，因此會花更多的時間去理解造成效率低",
+//                "https://www.google.com/");
+//        reviewMetricsDbManager.insertReviewMetrics(2, 1,
+//                "符合變數名稱格式",
+//                "審查人員需要找到一個對應的、真實的實質資料時，好的命名能減少審查人員的理解時間並提高效率",
+//                "https://www.google.com/");
+//        reviewMetricsDbManager.insertReviewMetrics(3, 1,
+//                "函式標示註解",
+//                "註解是\"解釋函式為什麼有時是有用的\"，而不是解釋函式裡面在做什麼，註解像是決策背後的原因",
+//                "https://www.google.com/");
+//        reviewMetricsDbManager.insertReviewMetrics(4, 1,
+//                "是否過度設計",
+//                "太複雜通常表示\"審查人員很難立即理解\"，這也表示\"開發人員在引用或修改此程式碼可能引入錯誤\"",
+//                "https://www.google.com/");
+    }
+
+    @Test
+    public void getReviewMetricsTest() {
+        List<ReviewMetrics> reviewMetricsList = reviewMetricsDbManager.getReviewMetrics(4);
+
+        for(ReviewMetrics reviewMetrics: reviewMetricsList) {
+            System.out.println(reviewMetrics.getId());
+            System.out.println(reviewMetrics.getCategory());
+            System.out.println(reviewMetrics.getMode());
+            System.out.println(reviewMetrics.getMetrics());
+            System.out.println(reviewMetrics.getDescription());
+            System.out.println(reviewMetrics.getLink());
+        }
+    }
+
+    @Test
+    public void editReviewMetricsByIdTest() {
+        reviewMetricsDbManager.editReviewMetricsById(8, 1, "editTest", "wwwwwwww");
+    }
+
+    @Test
+    public void deleteReviewMetricsTest() {
+        reviewMetricsDbManager.deleteReviewMetrics(9);
+    }
+}
