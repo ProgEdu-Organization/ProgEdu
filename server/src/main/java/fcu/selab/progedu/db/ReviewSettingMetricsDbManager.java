e61afc77e66127c50f03cb1bf073d9fec965771a
chenchunwei <chenchunwei24k@gmail.com>
2020-08-19 14:45:29 +0800

    Modified ReviewSettingMetricsDbManager

--- server/src/main/java/fcu/selab/progedu/db/ReviewSettingMetricsDbManager.java
+++ server/src/main/java/fcu/selab/progedu/db/ReviewSettingMetricsDbManager.java
@@ -1,24 +1,25 @@
 package fcu.selab.progedu.db;
 
+import fcu.selab.progedu.utils.ExceptionUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+public class ReviewSettingMetricsDbManager {
 
-import fcu.selab.progedu.utils.ExceptionUtil;
+    private static ReviewSettingMetricsDbManager dbManager = new ReviewSettingMetricsDbManager();
 
-public class AssignmentReviewMetricsDbManager {
-
-    private static AssignmentReviewMetricsDbManager dbManager = new AssignmentReviewMetricsDbManager();
-
-    public static AssignmentReviewMetricsDbManager getInstance() { return dbManager; }
+    public static ReviewSettingMetricsDbManager getInstance() { return dbManager; }
 
     private IDatabase database = new MySqlDatabase();
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(AssignmentReviewMetricsDbManager.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(ReviewSettingMetricsDbManager.class);
 
     /**
      * Insert review setting metrics into db
@@ -27,7 +28,17 @@ public class AssignmentReviewMetricsDbManager {
      * @param rmId              review metrics Id
      */
     public void insertReviewSettingMetrics(int rsId, int rmId) {
+        String query = "INSERT INTO Review_Setting_Metrics(rsId, rmId) VALUES(?,?)";
 
+        try (Connection conn = database.getConnection();
+             PreparedStatement preStmt = conn.prepareStatement(query)) {
+            preStmt.setInt(1, rsId);
+            preStmt.setInt(2, rmId);
+            preStmt.executeUpdate();
+        } catch (SQLException e) {
+            LOGGER.debug(ExceptionUtil.getErrorInfoFromException(e));
+            LOGGER.error(e.getMessage());
+        }
     }
 
     /**
@@ -35,11 +46,42 @@ public class AssignmentReviewMetricsDbManager {
      *
      * @param rsId              review setting Id
      */
+    public List<Integer> getReviewSettingMetricsByAssignmentId (int rsId) {
+        String query = "SELECT rmId FROM Review_Setting_Metrics WHERE rsId = ?";
+        List<Integer> metricsList = new ArrayList<>();
+
+        try (Connection conn = database.getConnection();
+             PreparedStatement preStmt = conn.prepareStatement(query)) {
+            preStmt.setInt(1, rsId);
+            try (ResultSet rs = preStmt.executeQuery()) {
+                while (rs.next()) {
+                    int rmId = rs.getInt("rmId");
+                    metricsList.add(rmId);
+                }
+            }
+        } catch (SQLException e) {
+            LOGGER.debug(ExceptionUtil.getErrorInfoFromException(e));
+            LOGGER.error(e.getMessage());
+        }
+        return metricsList;
+    }
 
     /**
      * Delete review metrics from review setting metrics by specific assignment id
      *
      * @param rsId              review setting Id
      */
+    public void deleteReviewSettingMetricsByAssignmentId(int rsId) {
+        String query = "DELETE FROM Review_Setting_Metrics WHERE rsId = ?";
+
+        try (Connection conn = database.getConnection();
+             PreparedStatement preStmt = conn.prepareStatement(query)) {
+            preStmt.setInt(1, rsId);
+            preStmt.executeUpdate();
+        } catch (SQLException e) {
+            LOGGER.debug(ExceptionUtil.getErrorInfoFromException(e));
+            LOGGER.error(e.getMessage());
+        }
+    }
 
 }
--- server/src/test/java/fcu/selab/progedu/service/ReviewSettingMetricsDbManagerTest.java
+++ server/src/test/java/fcu/selab/progedu/service/ReviewSettingMetricsDbManagerTest.java
@@ -1,2 +1,34 @@
-package fcu.selab.progedu.service;public class ReviewSettingMetricsDbManagerTest {
+package fcu.selab.progedu.service;
+
+import fcu.selab.progedu.db.ReviewSettingMetricsDbManager;
+import org.junit.Test;
+
+import java.util.List;
+
+public class ReviewSettingMetricsDbManagerTest {
+
+    ReviewSettingMetricsDbManager reviewSettingMetricsDbManager = new ReviewSettingMetricsDbManager();
+
+    @Test
+    public void insertReviewSettingMetricsTest() {
+        reviewSettingMetricsDbManager.insertReviewSettingMetrics(8, 2);
+        reviewSettingMetricsDbManager.insertReviewSettingMetrics(8, 4);
+        reviewSettingMetricsDbManager.insertReviewSettingMetrics(8, 6);
+        reviewSettingMetricsDbManager.insertReviewSettingMetrics(8, 7);
+        reviewSettingMetricsDbManager.insertReviewSettingMetrics(8, 8);
+    }
+
+    @Test
+    public void getReviewSettingMetricsByAssignmentIdTest() {
+        List<Integer> integerList = reviewSettingMetricsDbManager.getReviewSettingMetricsByAssignmentId(9);
+
+        for(Integer integer: integerList) {
+            System.out.println(integer);
+        }
+    }
+
+    @Test
+    public void deleteReviewSettingMetricsByAssignmentIdTest() {
+        reviewSettingMetricsDbManager.deleteReviewSettingMetricsByAssignmentId(9);
+    }
 }
