48ddf6ab76de3257b5f7a05d02a0b36e9e97b45e
raknza <llssf258@gmail.com>
2020-09-30 10:57:33 +0800

    Modified docker-compose

--- mongo/docker-entrypoint-initdb.d/init-mongo.sh
+++
@@ -1,4 +0,0 @@
-mongo ${MONGO_DATABASE} --eval "
-    db.initial.insert({'info':'build_success'});
-    db.createUser({ user: '${MONGO_USER}', pwd: '${MONGO_PASSWORD}', roles: [{ role: 'readWrite', db: '${MONGO_DATABASE}' }] });
-"
