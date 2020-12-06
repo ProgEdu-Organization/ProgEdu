a61f4dcb8775c0f8c02f05983519867fb03a5008
franky3020 <210238hwsh@gmail.com>
2020-12-06 16:23:25 +0800

    fix getJenkins script

--- getJenkinsUnlockPassword.sh
+++ getJenkinsUnlockPassword.sh
@@ -1,7 +1,13 @@
-#!/bin/bash
+#!/bin/sh
 
 if [ -f "./.env" ]; then
-    . ./.env
+
+    # replace all \r at end, becasue CRLF format in .env will cause error
+    sed -i 's/\r$//' .env
+    
+    # as same as `source .env` in bash, that is run .env then get the key/value 
+    . ./.env 
+    
     progedu_jenkins_container_name=${COMPOSE_PROJECT_NAME}_jenkins_1
     sudo docker exec -it ${progedu_jenkins_container_name} cat /var/jenkins_home/secrets/initialAdminPassword
 else
