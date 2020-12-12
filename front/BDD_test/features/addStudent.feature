f0dd24e116b604f108fcaccfbb3e9c792dee4b28
franky3020 <210238hwsh@gmail.com>
2020-12-12 21:37:41 +0800

    add other bdd

---
+++ front/BDD_test/features/addStudent.feature
@@ -0,0 +1,7 @@
+Feature: Add Student
+  test add Student
+
+  Scenario: Add Student
+    Given I have login
+    When navigate to add student page
+    Then I will see
--- front/BDD_test/features/step_definitions/addStudent.js
+++ front/BDD_test/features/step_definitions/addStudent.js
@@ -5,25 +5,27 @@ require('dotenv').config( {path:__dirname + '/../../.env'} ); // __dirname is th
 
 const { SingletDriver } = require(__dirname + '/util.js');
 
-let driver;
+let driver = SingletDriver.getDriver();
 
-Given('navigate to login page', function () {
-    driver =  SingletDriver.getDriver();
+Given('I have login', function () {
     driver.get(process.env["ProgEdu_HOST"]);
+
+
+
 });
 
-When('user logged in using username as {string} and password as {string}', function (username, password) {
+When('navigate to add student page', function () {
     const usernameElement = driver.findElement(By.id("username"));
     const passwordElement = driver.findElement(By.id("password"));
-    usernameElement.sendKeys(username);
-    passwordElement.sendKeys(password);
+    usernameElement.sendKeys("root");
+    passwordElement.sendKeys("rootrootrootroot");
     
     const loginButton = driver.findElement(By.xpath("//button[contains(text(),'Login')]"));
     loginButton.click();
 });
 
 // The default timeout in async function with cucumber is 5000ms  
-Then("I will see Hi,root", {timeout: 10 * 1000}, async function () {
+Then("I will see", {timeout: 10 * 1000}, async function () {
     
     const helloText = await driver.wait(until.elementLocated(By.xpath("//a[contains(text(),'Hi, root')]")), 10 * 1000);
     assert.strictEqual(await helloText.getText(), "Hi, root");
