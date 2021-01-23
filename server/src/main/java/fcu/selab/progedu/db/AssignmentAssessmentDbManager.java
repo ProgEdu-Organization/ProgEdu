2976b30358f88bca631cc49564bf7e45c965972e
YunHaoW <d0786880@mail.fcu.edu.tw>
2021-01-23 15:08:08 +0800

    delete unused import

--- server/src/main/java/fcu/selab/progedu/db/AssignmentAssessmentDbManager.java
+++ server/src/main/java/fcu/selab/progedu/db/AssignmentAssessmentDbManager.java
@@ -1,10 +1,8 @@
 package fcu.selab.progedu.db;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.Statement;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
