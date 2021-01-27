b105c45cd0c0f70f006477b455ec91abb8cb50b6
charlie <chentaolu@mail.fcu.edu.tw>
2021-01-27 21:29:45 +0800

    test

--- server/src/main/java/fcu/selab/progedu/setting/WebAssignmentSetting.java
+++ server/src/main/java/fcu/selab/progedu/setting/WebAssignmentSetting.java
@@ -65,11 +65,14 @@ public class WebAssignmentSetting implements AssignmentSettings {
         Node wannaDel = (Node) value2.getElementsByTagName
             ("hudson.plugins.build__timeout.BuildStepWithTimeout").item(1);
         value2.removeChild(wannaDel);
+        /*
         TransformerFactory transformerFactory = TransformerFactory.newInstance();
         Transformer transformer = transformerFactory.newTransformer();
         transformer.setOutputProperty(OutputKeys.INDENT,"yes");
         transformer.transform(
             new DOMSource(doc), new StreamResult(new File(configWebXmlPath)));
+
+         */
 
         BuildStepWithTimeoutCount = BuildStepWithTimeoutCount-2;
       }
