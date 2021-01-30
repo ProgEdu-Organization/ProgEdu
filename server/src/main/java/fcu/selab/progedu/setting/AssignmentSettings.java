331dbbe8a04ef530fcb53ac3fa804d5826bca259
franky3020 <210238hwsh@gmail.com>
2021-01-30 14:30:53 +0800

    some fix

--- server/src/main/java/fcu/selab/progedu/setting/AssignmentSettings.java
+++ server/src/main/java/fcu/selab/progedu/setting/AssignmentSettings.java
@@ -5,11 +5,17 @@ import org.apache.maven.model.Model;
 
 public interface AssignmentSettings {
 
+  // Todo 以下改成 void createAssignmentSetting(List<String> order, String name, String targetPomSavePath);
+  // Todo targetPath 是即將要生成的 pom.xml檔 儲存路徑, 這只需要pom檔就好, 整份專案只有在下載時在去組合, 所以不用解壓所整份專案
+  // Todo 到 assignmentSetting 資料夾
   public void createAssignmentSetting(List<String> order,String name);
 
+  // Todo 刪掉以下這行
   public void unZipAssignmentToTmp();
 
+  // Todo 刪掉以下這行
   public void packUpAssignment();
 
+  // Todo 刪掉以下這行
   public void writeAssignmentSettingFile();
 }
