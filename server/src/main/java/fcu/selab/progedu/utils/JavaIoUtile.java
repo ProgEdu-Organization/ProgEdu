1ac1650b84b114b802ee371015ba09c60329b1d8
franky3020 <210238hwsh@gmail.com>
2021-01-31 18:24:30 +0800

    clean code

--- server/src/main/java/fcu/selab/progedu/utils/JavaIoUtile.java
+++ server/src/main/java/fcu/selab/progedu/utils/JavaIoUtile.java
@@ -1,11 +1,9 @@
 package fcu.selab.progedu.utils;
 
-import fcu.selab.progedu.conn.TomcatService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.*;
-import java.nio.file.Files;
 
 public class JavaIoUtile {
 
