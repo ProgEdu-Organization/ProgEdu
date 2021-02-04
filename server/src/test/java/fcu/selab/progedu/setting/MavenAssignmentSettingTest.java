f957e76918f4e8fdd63445bbd790891bcc5f83ac
charlie <chentaolu@mail.fcu.edu.tw>
2021-02-04 19:23:31 +0800

    create maven pom test

--- server/src/test/java/fcu/selab/progedu/setting/MavenAssignmentSettingTest.java
+++ server/src/test/java/fcu/selab/progedu/setting/MavenAssignmentSettingTest.java
@@ -1,7 +1,47 @@
 package fcu.selab.progedu.setting;
 
 import junit.framework.TestCase;
+import org.apache.commons.logging.Log;
+import org.junit.Test;
+import fcu.selab.progedu.setting.MavenAssignmentSetting;
+import org.w3c.dom.Document;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MavenAssignmentSettingTest extends TestCase {
 
+  @Test
+  public void testCreateSetting() {
+    String saveSettingPath = System.getProperty("java.io.tmpdir")
+        + "/mavenPomXmlSetting/";
+
+    List<String> orderList = new ArrayList<>();
+    orderList.add("Compile Failure");
+    orderList.add("Coding Style Failure");
+    orderList.add("Unit Test Failure");
+
+    MavenAssignmentSetting mas = new MavenAssignmentSetting();
+    mas.createAssignmentSetting(orderList,
+        "unit-test-for-create-maven-setting", saveSettingPath);
+
+    try {
+      File inputFile = new File(saveSettingPath
+          + "unit-test-for-create-maven-setting_pom.xml");
+      DocumentBuilderFactory docFactory =
+          DocumentBuilderFactory.newInstance();
+      DocumentBuilder docBuilder =
+          docFactory.newDocumentBuilder();
+      Document doc = docBuilder.parse(inputFile);
+      doc.getDocumentElement().normalize();
+
+      System.out.println(doc);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+
+  }
 }
