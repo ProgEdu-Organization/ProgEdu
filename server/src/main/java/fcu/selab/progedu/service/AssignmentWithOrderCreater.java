aa02fe3d3572a4411727170870f12e6278083b1e
chentaolu <chentaolu@mail.fcu.edu.tw>
2021-06-06 16:11:45 +0800

    fix  bug

--- server/src/main/java/fcu/selab/progedu/service/AssignmentWithOrderCreater.java
+++ server/src/main/java/fcu/selab/progedu/service/AssignmentWithOrderCreater.java
@@ -37,230 +37,235 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-public class AssignmentWithOrderCreater {
+public class AssignmentWithOrderCreator {
+  private ZipHandler zipHandler;
+  private GitlabService gitlabService = GitlabService.getInstance();
+  private TomcatService tomcatService = TomcatService.getInstance();
+  private UserService userService = UserService.getInstance();
+  private String gitlabRootUsername;
+  private CourseConfig courseConfig = CourseConfig.getInstance();
+  private GitlabConfig gitlabData = GitlabConfig.getInstance();
+  private JenkinsConfig jenkinsData = JenkinsConfig.getInstance();
 
-    private ZipHandler zipHandler;
-    private GitlabService gitlabService = GitlabService.getInstance();
-    private TomcatService tomcatService = TomcatService.getInstance();
-    private UserService userService = UserService.getInstance();
-    private String gitlabRootUsername;
-    private CourseConfig courseConfig = CourseConfig.getInstance();
-    private GitlabConfig gitlabData = GitlabConfig.getInstance();
-    private JenkinsConfig jenkinsData = JenkinsConfig.getInstance();
+  private AssignmentDbManager adbManager = AssignmentDbManager.getInstance();
+  private UserDbManager userDbManager = UserDbManager.getInstance();
+  private AssignmentUserDbManager auDbManager = AssignmentUserDbManager.getInstance();
+  private AssignmentAssessmentDbManager aaDbManager = AssignmentAssessmentDbManager.getInstance();
+  private static final Logger LOGGER = LoggerFactory.getLogger(AssignmentWithOrderCreator.class);
 
-    private AssignmentDbManager adbManager = AssignmentDbManager.getInstance();
-    private UserDbManager userDbManager = UserDbManager.getInstance();
-    private AssignmentUserDbManager auDbManager = AssignmentUserDbManager.getInstance();
-    private AssignmentAssessmentDbManager aaDbManager = AssignmentAssessmentDbManager.getInstance();
-    private static final Logger LOGGER = LoggerFactory.getLogger(AssignmentWithOrderCreater.class);
+  private final String projectDir = System.getProperty("catalina.base");
+  private final String tempDir = System.getProperty("java.io.tmpdir");
+  private final String uploadDir = tempDir + "/uploads/";
+  private final String imageTempName = "/temp_images/";
+  private final String imageTempDir = projectDir + imageTempName;
 
-    private final String projectDir = System.getProperty("catalina.base");
-    private final String tempDir = System.getProperty("java.io.tmpdir");
-    private final String uploadDir = tempDir + "/uploads/";
-    private final String imageTempName = "/temp_images/";
-    private final String imageTempDir = projectDir + imageTempName;
+  private List<String> instructionList = new ArrayList<>();
 
-    public AssignmentWithOrderCreater() {
-        try {
-            zipHandler = new ZipHandler();
-            gitlabRootUsername = gitlabData.getGitlabRootUsername();
-        } catch (LoadConfigFailureException e) {
-            LOGGER.debug(ExceptionUtil.getErrorInfoFromException(e));
-            LOGGER.error(e.getMessage());
-        }
+  public AssignmentWithOrderCreator() {
+      try {
+          zipHandler = new ZipHandler();
+          gitlabRootUsername = gitlabData.getGitlabRootUsername();
+      } catch (LoadConfigFailureException e) {
+          LOGGER.debug(ExceptionUtil.getErrorInfoFromException(e));
+          LOGGER.error(e.getMessage());
+      }
+  }
+
+  public Response createAssignment(
+      @FormDataParam("assignmentName") String assignmentName,
+      @FormDataParam("releaseTime") Date releaseTime, @FormDataParam("deadline") Date deadline,
+      @FormDataParam("readMe") String readMe, @FormDataParam("fileRadio") String assignmentType,
+      @FormDataParam("file") InputStream file,
+      @FormDataParam("file") FormDataContentDisposition fileDetail) {
+
+
+    // 1. Create root project and get project id and url
+    gitlabService.createRootProject(assignmentName);
+
+    // 2. Clone the project
+    final String cloneDirectoryPath = gitlabService.cloneProject(gitlabRootUsername,
+        assignmentName);
+    // 3. Store Zip File to uploads folder if file is not empty
+    String filePath = tomcatService.storeFileToUploadsFolder(file, fileDetail.getFileName());
+
+    // 4. Unzip the uploaded file to cloneDirectoryPath
+    zipHandler.unzipFile(filePath, cloneDirectoryPath);
+
+    // 5. Add .gitkeep if folder is empty.
+    JavaIoUtile.addFile2EmptyFolder(new File(cloneDirectoryPath), ".gitkeep");
+
+    // 6. Copy all description image to temp/images
+    LOGGER.debug("Copy all description image to temp/images");
+    LOGGER.debug("System.getProperty('catalina.base') is ");
+    LOGGER.debug(System.getProperty("catalina.base"));
+
+    ArrayList<String> paths = findAllDescriptionImagePaths(readMe);
+    for (String path : paths) {
+      String targetPath = path.replace("temp_images", "images");
+
+      File originalFile = new File(projectDir + path);
+      File targetFile = new File(projectDir + targetPath);
+      LOGGER.debug("start copy :" + originalFile.getPath());
+      LOGGER.debug("to :" + targetFile.getPath());
+
+      try {
+        JavaIoUtile.copyDirectoryCompatibilityMode(originalFile, targetFile);
+      } catch (Exception e) {
+        LOGGER.debug(ExceptionUtil.getErrorInfoFromException(e));
+      LOGGER.error(e.getMessage());
+      }
     }
 
-    public Response createAssignment(
-            @FormDataParam("assignmentName") String assignmentName,
-            @FormDataParam("releaseTime") Date releaseTime, @FormDataParam("deadline") Date deadline,
-            @FormDataParam("readMe") String readMe, @FormDataParam("fileRadio") String assignmentType,
-            @FormDataParam("file") InputStream file,
-            @FormDataParam("file") FormDataContentDisposition fileDetail) {
+      // Delete all images of temp_images folder, but not temp_images folder
+    JavaIoUtile.deleteFileInDirectory(new File(imageTempDir));
 
-
-        // 1. Create root project and get project id and url
-        gitlabService.createRootProject(assignmentName);
-
-        // 2. Clone the project
-        final String cloneDirectoryPath = gitlabService.cloneProject(gitlabRootUsername,
-                assignmentName);
-//    // 3. Store Zip File to uploads folder if file is not empty
-        String filePath = tomcatService.storeFileToUploadsFolder(file, fileDetail.getFileName());
-
-        // 4. Unzip the uploaded file to cloneDirectoryPath
-        zipHandler.unzipFile(filePath, cloneDirectoryPath);
-
-        // 5. Add .gitkeep if folder is empty.
-        JavaIoUtile.addFile2EmptyFolder(new File(cloneDirectoryPath), ".gitkeep");
-
-        // 6. Copy all description image to temp/images
-        LOGGER.debug("Copy all description image to temp/images");
-        LOGGER.debug("System.getProperty('catalina.base') is ");
-        LOGGER.debug(System.getProperty("catalina.base"));
-
-        ArrayList<String> paths = findAllDescriptionImagePaths(readMe);
-        for (String path : paths) {
-            String targetPath = path.replace("temp_images", "images");
-
-            File originalFile = new File(projectDir + path);
-            File targetFile = new File(projectDir + targetPath);
-            LOGGER.debug("start copy :" + originalFile.getPath());
-            LOGGER.debug("to :" + targetFile.getPath());
-
-            try {
-                JavaIoUtile.copyDirectoryCompatibilityMode(originalFile, targetFile);
-            } catch (Exception e) {
-                LOGGER.debug(ExceptionUtil.getErrorInfoFromException(e));
-                LOGGER.error(e.getMessage());
-            }
-        }
-
-        // Delete all images of temp_images folder, but not temp_images folder
-        JavaIoUtile.deleteFileInDirectory(new File(imageTempDir));
-
-        // 7. If README is not null
-        // First, we need to modify images path
-        try {
-            readMe = readMe.replaceAll(imageTempName, courseConfig.getTomcatServerIp() + "/images/");
-            if (!readMe.equals("<br>") || !"".equals(readMe) || !readMe.isEmpty()) {
-                // Add readme to folder
-                JavaIoUtile.createUtf8FileFromString(readMe, new File(cloneDirectoryPath, "README.md"));
-            }
-        } catch (LoadConfigFailureException e) {
-            LOGGER.debug(ExceptionUtil.getErrorInfoFromException(e));
-            LOGGER.error(e.getMessage());
-        }
-
-        // 8. git push
-        gitlabService.pushProject(cloneDirectoryPath);
-
-        // 9. import project information to database
-        ProjectTypeEnum projectTypeEnum = ProjectTypeEnum.getProjectTypeEnum(assignmentType);
-        addProject(assignmentName, releaseTime, deadline, readMe, projectTypeEnum);
-
-
-        List<User> users = userService.getStudents();
-        for (User user : users) {
-            createAssignmentSettingsV2(user.getUsername(), assignmentName);
-        }
-
-        // 10. remove project file
-        JavaIoUtile.deleteDirectory(new File(uploadDir));
-        return Response.ok().build();
+      // 7. If README is not null
+      // First, we need to modify images path
+    try {
+      readMe = readMe.replaceAll(imageTempName, courseConfig.getTomcatServerIp() + "/images/");
+      if (!readMe.equals("<br>") || !"".equals(readMe) || !readMe.isEmpty()) {
+        // Add readme to folder
+        JavaIoUtile.createUtf8FileFromString(readMe, new File(cloneDirectoryPath, "README.md"));
+      }
+    } catch (LoadConfigFailureException e) {
+      LOGGER.debug(ExceptionUtil.getErrorInfoFromException(e));
+      LOGGER.error(e.getMessage());
     }
 
-    private void createAssignmentSettingsV2(String username, String assignmentName) {
+      // 8. git push
+    gitlabService.pushProject(cloneDirectoryPath);
 
-        addAuid(username, assignmentName);
-        //Todo 以上 addAuid 要改, 因為之後沒有 assignment
+      // 9. import project information to database
+    ProjectTypeEnum projectTypeEnum = ProjectTypeEnum.getProjectTypeEnum(assignmentType);
+    addProject(assignmentName, releaseTime, deadline, readMe, projectTypeEnum);
 
-        GitlabProject gitlabProject = gitlabService.createPrivateProject(username,
-                assignmentName, "root"); // Todo assignment 要改
 
-        try {
-            String jobName = username + "_" + assignmentName;
+    List<User> users = userService.getStudents();
+    for (User user : users) {
+      createAssignmentSettingsV2(user.getUsername(), assignmentName);
+    }
 
-            gitlabService.setGitlabWebhook(gitlabProject, jobName);
+      // 10. remove project file
+    JavaIoUtile.deleteDirectory(new File(uploadDir));
+    return Response.ok().build();
+  }
 
-            GitlabConfig gitlabConfig = GitlabConfig.getInstance();
-            String projectUrl = gitlabConfig.getGitlabHostUrl() + "/" + username + "/" + assignmentName
-                    + ".git";
+  private void createAssignmentSettingsV2(String username, String assignmentName) {
 
-            CourseConfig courseConfig = CourseConfig.getInstance();
-            String progEduApiUrl = courseConfig.getTomcatServerIp() + courseConfig.getBaseuri()
-                    + "/webapi";
-            String updateDbUrl = courseConfig.getTomcatServerIp() + "/publicApi/update/commits";
+    addAuid(username, assignmentName);
+    //Todo 以上 addAuid 要改, 因為之後沒有 assignment
 
-            //
-            List<String> ordersList = new ArrayList<>();
-            String[] ordersAndScores = aaDbManager.getAssignmentOrderAndScore(
-                    adbManager.getAssignmentIdByName(assignmentName)).split(", ");
-            while (ordersList.size() == 0) {
-                ordersAndScores = aaDbManager.getAssignmentOrderAndScore(
-                        adbManager.getAssignmentIdByName(assignmentName)).split(", ");
-            }
-            for (String orderAndScore : ordersAndScores) {
-                String[] token = orderAndScore.split(":");
-                ordersList.add(token[0]);
-            }
-            String orderString = "";
-            if (ordersList.isEmpty() != true) {
-                for (int i = 0; i < ordersList.size(); i++) {
-                    orderString += ordersList.get(i);
-                    if (i < ordersList.size() - 1) {
-                        orderString += ", ";
-                    }
-                }
-            }
-            //
-            ProjectTypeEnum assignmentTypeEnum = adbManager.getAssignmentType(assignmentName);
-            JenkinsProjectConfig jenkinsProjectConfig;
-            if ( assignmentTypeEnum.equals(ProjectTypeEnum.WEB) ) {
-                jenkinsProjectConfig = new WebPipelineConfig(projectUrl, updateDbUrl,
-                        username, assignmentName,
-                        courseConfig.getTomcatServerIp() + "/publicApi/commits/screenshot/updateURL",
-                        orderString);
-            } else {
-                jenkinsProjectConfig = JenkinsProjectConfigFactory
-                        .getJenkinsProjectConfig(assignmentTypeEnum.getTypeName(), projectUrl, updateDbUrl,
-                                username, assignmentName);
-            }
-            JenkinsService jenkinsService = JenkinsService.getInstance();
-            jenkinsService.createJobV2(jobName, jenkinsProjectConfig.getXmlConfig());
-            jenkinsService.buildJob(jobName);
+    GitlabProject gitlabProject = gitlabService.createPrivateProject(username,
+        assignmentName, "root"); // Todo assignment 要改
 
-        } catch (Exception e) {
-            LOGGER.debug(ExceptionUtil.getErrorInfoFromException(e));
-            LOGGER.error(e.getMessage());
+    try {
+      String jobName = username + "_" + assignmentName;
+
+      gitlabService.setGitlabWebhook(gitlabProject, jobName);
+
+      GitlabConfig gitlabConfig = GitlabConfig.getInstance();
+      String projectUrl = gitlabConfig.getGitlabHostUrl() + "/" + username + "/" + assignmentName
+          + ".git";
+
+      CourseConfig courseConfig = CourseConfig.getInstance();
+      String progEduApiUrl = courseConfig.getTomcatServerIp() + courseConfig.getBaseuri()
+          + "/webapi";
+      String updateDbUrl = courseConfig.getTomcatServerIp() + "/publicApi/update/commits";
+
+      //
+      List<String> ordersList = new ArrayList<>();
+      String[] ordersAndScores = aaDbManager.getAssignmentOrderAndScore(
+          adbManager.getAssignmentIdByName(assignmentName)).split(", ");
+      while (ordersList.size() == 0) {
+          ordersAndScores = aaDbManager.getAssignmentOrderAndScore(
+              adbManager.getAssignmentIdByName(assignmentName)).split(", ");
+      }
+      for (String orderAndScore : ordersAndScores) {
+          String[] token = orderAndScore.split(":");
+          ordersList.add(token[0]);
+      }
+      String orderString = "";
+      if (ordersList.isEmpty() != true) {
+        for (int i = 0; i < ordersList.size(); i++) {
+          orderString += ordersList.get(i);
+          if (i < ordersList.size() - 1) {
+            orderString += ", ";
+          }
         }
+      }
+      //
+      ProjectTypeEnum assignmentTypeEnum = adbManager.getAssignmentType(assignmentName);
+      JenkinsProjectConfig jenkinsProjectConfig;
+      if ( assignmentTypeEnum.equals(ProjectTypeEnum.WEB) ) {
+          jenkinsProjectConfig = new WebPipelineConfig(projectUrl, updateDbUrl,
+              username, assignmentName,
+                  courseConfig.getTomcatServerIp() + "/publicApi/commits/screenshot/updateURL",
+                      orderString);
+      } else {
+        jenkinsProjectConfig = JenkinsProjectConfigFactory
+            .getJenkinsProjectConfig(assignmentTypeEnum.getTypeName(), projectUrl, updateDbUrl,
+                username, assignmentName);
+      }
+      JenkinsService jenkinsService = JenkinsService.getInstance();
+      jenkinsService.createJobV2(jobName, jenkinsProjectConfig.getXmlConfig());
+      jenkinsService.buildJob(jobName);
+
+    } catch (Exception e) {
+      LOGGER.debug(ExceptionUtil.getErrorInfoFromException(e));
+      LOGGER.error(e.getMessage());
     }
+  }
 
-    /**
-     * Add auid to database
-     *
-     * @param username       username
-     * @param assignmentName assignment name
-     */
-    public void addAuid(String username, String assignmentName) {
-        int aid = adbManager.getAssignmentIdByName(assignmentName);
-        int uid = userDbManager.getUserIdByUsername(username);
+  /**
+   * Add auid to database
+   *
+   * @param username       username
+   * @param assignmentName assignment name
+   */
+  public void addAuid(String username, String assignmentName) {
+    int aid = adbManager.getAssignmentIdByName(assignmentName);
+    int uid = userDbManager.getUserIdByUsername(username);
 
-        auDbManager.addAssignmentUser(aid, uid);
+    auDbManager.addAssignmentUser(aid, uid);
+  }
+
+  /**
+   * Add a project to database
+   *
+   * @param name        Project name
+   * @param deadline    Project deadline
+   * @param readMe      Project readme
+   * @param projectType File type
+   */
+  public void addProject(String name, Date releaseTime, Date deadline, String readMe,
+      ProjectTypeEnum projectType) {
+    Assignment assignment = new Assignment();
+    Date date = tomcatService.getCurrentTime();
+    assignment.setName(name);
+    assignment.setCreateTime(date);
+    assignment.setReleaseTime(releaseTime);
+    assignment.setDeadline(deadline);
+    assignment.setDescription(readMe);
+    assignment.setType(projectType);
+
+    adbManager.addAssignment(assignment);
+  }
+
+  /**
+   * @param readme Description details
+   * @return All images paths
+   */
+  public ArrayList<String> findAllDescriptionImagePaths(String readme) {
+    ArrayList<String> paths = new ArrayList<>();
+    Document doc = Jsoup.parse(readme);
+    Elements srcAttributes = doc.select("img[src]");
+    for (Element src : srcAttributes) {
+      paths.add(src.attr("src"));
     }
+    return paths;
+  }
 
-    /**
-     * Add a project to database
-     *
-     * @param name        Project name
-     * @param deadline    Project deadline
-     * @param readMe      Project readme
-     * @param projectType File type
-     */
-    public void addProject(String name, Date releaseTime, Date deadline, String readMe,
-                           ProjectTypeEnum projectType) {
-        Assignment assignment = new Assignment();
-        Date date = tomcatService.getCurrentTime();
-        assignment.setName(name);
-        assignment.setCreateTime(date);
-        assignment.setReleaseTime(releaseTime);
-        assignment.setDeadline(deadline);
-        assignment.setDescription(readMe);
-        assignment.setType(projectType);
-
-        adbManager.addAssignment(assignment);
-    }
-
-    /**
-     * @param readme Description details
-     * @return All images paths
-     */
-    public ArrayList<String> findAllDescriptionImagePaths(String readme) {
-        ArrayList<String> paths = new ArrayList<>();
-        Document doc = Jsoup.parse(readme);
-        Elements srcAttributes = doc.select("img[src]");
-        for (Element src : srcAttributes) {
-            paths.add(src.attr("src"));
-        }
-        return paths;
-    }
+  public void appendOrder(String instruction) {
+    instructionList.add(instruction);
+  }
 }
