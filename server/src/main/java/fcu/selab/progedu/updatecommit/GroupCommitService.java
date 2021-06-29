67bfca9f81bcb47a6da50cee0ed482baf74a39e1
chentaolu <chentaolu@mail.fcu.edu.tw>
2021-06-29 21:27:26 +0800

    fix bug

--- server/src/main/java/fcu/selab/progedu/updatecommit/GroupCommitService.java
+++ server/src/main/java/fcu/selab/progedu/updatecommit/GroupCommitService.java
@@ -15,7 +15,11 @@ import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.ws.rs.*;
+import javax.ws.rs.POST;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.text.DateFormat;
