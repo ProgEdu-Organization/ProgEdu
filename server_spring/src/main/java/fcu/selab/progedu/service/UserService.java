d6cb1eb51a4f814824f91c5457d9e8ab8df4f62c
franky3020 <210238hwsh@gmail.com>
2021-06-29 09:15:38 +0800

    完成遷移getUser

--- server_spring/src/main/java/fcu/selab/progedu/service/UserService.java
+++ server_spring/src/main/java/fcu/selab/progedu/service/UserService.java
@@ -1,20 +1,17 @@
 package fcu.selab.progedu.service;
 
-import fcu.selab.progedu.Greeting;
 import fcu.selab.progedu.data.User;
 import fcu.selab.progedu.db.UserDbManager;
-import org.json.JSONObject;
+import net.minidev.json.JSONObject;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping(value ="/user")
@@ -23,15 +20,43 @@ public class UserService {
   private UserDbManager dbManager = UserDbManager.getInstance();
 
   @GetMapping("/getUsers")
-  public Map<String, Object> getUsers() {
+  public ResponseEntity<Object> getUsers() {
     HttpHeaders headers = new HttpHeaders();
     headers.add("Content-Type", "application/json");
 
     List<User> users = dbManager.getAllUsers();
+    List<JSONObject> userListEntities = new ArrayList<>();
 
-    Map<String, Object> entities = new HashMap<String, Object>();
-    entities.put("Users", users);
-    return entities;
+    for (User user : users) {
+      JSONObject entity = new JSONObject();
+      entity.put("gitLabId", user.getGitLabId());
+      entity.put("password", user.getPassword());
+
+
+      List<JSONObject> roleList = new ArrayList<>();
+      for (RoleEnum userRule : user.getRole()) {
+        JSONObject typeName = new JSONObject();
+        typeName.put("typeName", userRule.getTypeName());
+        roleList.add(typeName);
+      }
+
+      entity.put("role", roleList);
+
+      entity.put("gitLabToken", user.getGitLabToken());
+      entity.put("display", user.getDisplay());
+      entity.put("name", user.getName());
+      entity.put("id", user.getId());
+      entity.put("email", user.getEmail());
+      entity.put("username", user.getUsername());
+
+      userListEntities.add(entity);
+    }
+
+    JSONObject rootUserEntity = new JSONObject();
+    rootUserEntity.put("User", userListEntities);
+
+
+    return new ResponseEntity<Object>(rootUserEntity, headers, HttpStatus.OK);
   }
 
 }
