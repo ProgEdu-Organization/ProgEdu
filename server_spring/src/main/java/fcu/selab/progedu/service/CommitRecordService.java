ac3042da39484b94c55a4ba6aed05ef48326e212
franky3020 <210238hwsh@gmail.com>
2021-07-07 17:06:59 +0800

    加空白類別

---
+++ server_spring/src/main/java/fcu/selab/progedu/service/CommitRecordService.java
@@ -0,0 +1,24 @@
+package fcu.selab.progedu.service;
+
+
+public class CommitRecordService {
+
+  public void getPartCommitRecord() {
+
+  }
+
+  public void getAutoAssessment() {
+
+  }
+
+  // getAssignmentFeedback
+  public void getFeedback() {
+
+  }
+
+  // getAllStudentCommitRecord
+  public void getAllUsersCommitRecord() {
+
+  }
+
+}
---
+++ server_spring/src/main/java/fcu/selab/progedu/service/GroupCommitRecordService.java
@@ -0,0 +1,16 @@
+package fcu.selab.progedu.service;
+
+public class GroupCommitRecordService {
+
+  // groupsCommit
+  public void getAllGroupCommitRecord() {
+
+  }
+
+  // userGroupCommit
+  public void getCommitRecordByUsername() {
+
+  }
+
+
+}
