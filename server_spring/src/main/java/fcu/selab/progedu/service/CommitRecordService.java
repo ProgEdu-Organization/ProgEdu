0b3fc2e79e23b2f1adae956833ce6eb2869bc6fe
chentaolu <chentaolu@mail.fcu.edu.tw>
2021-07-08 18:24:13 +0800

    add commits/gitLab in to spring server

--- server_spring/src/main/java/fcu/selab/progedu/service/CommitRecordService.java
+++ server_spring/src/main/java/fcu/selab/progedu/service/CommitRecordService.java
@@ -1,7 +1,34 @@
 package fcu.selab.progedu.service;
 
+import net.minidev.json.JSONObject;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import fcu.selab.progedu.conn.GitlabService;
+
+
+@RestController
+@RequestMapping(value = "/commits")
 public class CommitRecordService {
+
+  private GitlabService gitlabService = GitlabService.getInstance();
+
+  @GetMapping("/gitLab")
+  public ResponseEntity<Object> getGitLabURL(
+          @RequestParam("username") String username,
+          @RequestParam("assignmentName") String assignmentName) {
+    HttpHeaders headers = new HttpHeaders();
+    headers.add("Content-Type", "application/json");
+    
+    String projectUrl = gitlabService.getProjectUrl(username, assignmentName);
+
+    JSONObject gitLabEntity = new JSONObject();
+    gitLabEntity.put("url", projectUrl);
+
+    return new ResponseEntity<Object>(gitLabEntity, headers, HttpStatus.OK);
+  }
 
   public void getPartCommitRecord() {
 
