f221a43dd95d8b4ecb9a6161f26c0e704bf55596
YunHaoW <d0786880@mail.fcu.edu.tw>
2021-07-19 23:13:39 +0800

    finish API : /peerReview/status/allUsers

--- server_spring/src/main/java/fcu/selab/progedu/service/PeerReviewService.java
+++ server_spring/src/main/java/fcu/selab/progedu/service/PeerReviewService.java
@@ -1,17 +1,23 @@
 package fcu.selab.progedu.service;
 
+import javax.ws.rs.core.Response;
 import fcu.selab.progedu.data.PairMatching;
 import fcu.selab.progedu.data.ReviewSetting;
 import fcu.selab.progedu.data.Assignment;
+import fcu.selab.progedu.data.User;
 import fcu.selab.progedu.db.AssignmentDbManager;
 import fcu.selab.progedu.db.ReviewSettingDbManager;
 import fcu.selab.progedu.db.PairMatchingDbManager;
 import fcu.selab.progedu.db.UserDbManager;
 
+import fcu.selab.progedu.utils.ExceptionUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Date;
 import java.sql.SQLException;
@@ -27,7 +33,13 @@ public class PeerReviewService {
   private ReviewSettingDbManager reviewSettingDbManager = ReviewSettingDbManager.getInstance();
   private PairMatchingDbManager pairMatchingDbManager = PairMatchingDbManager.getInstance();
   private UserDbManager userDbManager = UserDbManager.getInstance();
+  private static final Logger LOGGER = LoggerFactory.getLogger(PeerReviewService.class);
 
+  /**
+   * get one user's status of reviewing other's hw
+   *
+   * @param username user name
+   */
 	@GetMapping("/status/oneUser")
 	public ResponseEntity<Object> getReviewStatus(
 					@RequestParam("username") String username) {
@@ -58,11 +70,43 @@ public class PeerReviewService {
 				jsonArray.add(jsonObject);
 			}
     	return new ResponseEntity<Object>(jsonArray, headers, HttpStatus.OK);
-
 		} catch (Exception e) {
 			return new ResponseEntity<Object>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
+
+  /**
+   * get all user's status of reviewing other's hw
+   */
+  @GetMapping("/status/allUsers")
+  public ResponseEntity<Object> getAllReviewStatus() {
+
+    HttpHeaders headers = new HttpHeaders();
+    headers.add("Content-Type", "application/json");
+
+    try {
+      JSONArray array = new JSONArray();
+      JSONObject result = new JSONObject();
+      List<User> users = getStudents();
+      for (User user : users) {
+        String username = user.getUsername();
+        ResponseEntity<Object> reviewStatus = getReviewStatus(username);
+        JSONObject ob = new JSONObject();
+        ob.put("username", username);
+        ob.put("name", user.getName());
+        ob.put("display", user.getDisplay());
+        ob.put("reviewStatus", reviewStatus.getBody());
+
+        array.add(ob);
+      }
+      result.put("allReviewStatus", array);
+      return new ResponseEntity<Object>(result, headers, HttpStatus.OK);
+    } catch (Exception e){
+      LOGGER.debug(ExceptionUtil.getErrorInfoFromException(e));
+      LOGGER.error(e.getMessage());
+      return new ResponseEntity<Object>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
 
 	private int getReviewCompletedCount(int aid, int reviewId) throws SQLException {
     List<PairMatching> pairMatchingList =
@@ -106,5 +150,22 @@ public class PeerReviewService {
     }
 
     return resultStatus;
+  }
+
+  /**
+   * Get all user which role is student
+   *
+   * @return all GitLab users
+   */
+  public List<User> getStudents() {
+    List<User> studentUsers = new ArrayList<>();
+    List<User> users = userDbManager.getAllUsers();
+
+    for (User user : users) {
+      if (user.getRole().contains(RoleEnum.STUDENT)) {
+        studentUsers.add(user);
+      }
+    }
+    return studentUsers;
   }
 }
