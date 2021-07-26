84ed67ee99ef095786e24c3948c33aa59ff7b9af
ChuanLin Huang <qwer23655441@gmail.com>
2021-07-27 00:05:50 +0800

    遷移 getCommitRecord api

--- server_spring/src/main/java/fcu/selab/progedu/service/GroupCommitRecordService.java
+++ server_spring/src/main/java/fcu/selab/progedu/service/GroupCommitRecordService.java
@@ -2,6 +2,8 @@ package fcu.selab.progedu.service;
 
 import java.text.ParseException;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Date;
 import fcu.selab.progedu.conn.JenkinsService;
 import fcu.selab.progedu.data.ProjectTypeEnum;
 import fcu.selab.progedu.db.UserDbManager;
@@ -11,6 +13,9 @@ import fcu.selab.progedu.db.service.ProjectGroupDbService;
 import fcu.selab.progedu.status.Status;
 import fcu.selab.progedu.status.StatusAnalysisFactory;
 import fcu.selab.progedu.status.StatusEnum;
+import fcu.selab.progedu.data.CommitRecord;
+import net.minidev.json.JSONObject;
+import net.minidev.json.JSONArray;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpHeaders;
@@ -64,6 +69,40 @@ public class GroupCommitRecordService {
     String feedBackMessage = statusAnalysis.tojsonArray(feedBacks);
 
     return new ResponseEntity<Object>(feedBackMessage, headers, HttpStatus.OK);
+  }
+  /**
+   * get student build detail info
+   *
+   * @param groupName student id
+   * @param projectName assignment name
+   * @return build detail
+   */
+
+  @GetMapping("/{name}/projects/{projectName}/commits")
+  public ResponseEntity<Object> getCommitRecord(
+          @PathVariable("name") String groupName, @PathVariable("projectName") String projectName){
+    JenkinsService js = JenkinsService.getInstance();
+    List<JSONObject> array = new ArrayList<>();
+    int pgid = gpdb.getPgid(groupName, projectName);
+    List<CommitRecord> commitRecords = gpdb.getCommitRecords(pgid);
+    String jobName = groupName + "_" + projectName;
+    for (CommitRecord commitRecord : commitRecords) {
+      int number = commitRecord.getNumber();
+      String message = js.getCommitMessage(jobName, number);
+      Date time = commitRecord.getTime();
+      String status = commitRecord.getStatus().getType();
+      String committer = commitRecord.getCommitter();
+
+      JSONObject ob = new JSONObject();
+
+      ob.put("number", number);
+      ob.put("status", status.toUpperCase());
+      ob.put("time", time.toString());
+      ob.put("message", message);
+      ob.put("committer", committer);
+      array.add(ob);
+    }
+    return new ResponseEntity<>(array.toString(), HttpStatus.OK);
   }
 
   // groupsCommit
