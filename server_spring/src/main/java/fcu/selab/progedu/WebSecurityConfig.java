1f9cbcb3a8782048c749e56bc75873765d11efb3
franky3020 <210238hwsh@gmail.com>
2021-08-24 17:29:28 +0800

    最簡易JWT

---
+++ server_spring/src/main/java/fcu/selab/progedu/WebSecurityConfig.java
@@ -0,0 +1,43 @@
+package fcu.selab.progedu;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+  @Bean
+  PasswordEncoder passwordEncoder() {
+    return NoOpPasswordEncoder.getInstance();
+  }
+  @Override
+  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    auth.inMemoryAuthentication().withUser("franky")
+            .password("franky-password").roles("");
+//            .and()
+//            .withUser("sang")
+//            .password("456")
+//            .roles("user");
+  }
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.authorizeRequests()
+            .antMatchers("/hello").hasRole("user")
+            .antMatchers("/admin").hasRole("admin")
+            .antMatchers(HttpMethod.POST, "/login").permitAll()
+            .anyRequest().authenticated()
+            .and()
+            .addFilterBefore(new JwtLoginFilter("/login",authenticationManager()), UsernamePasswordAuthenticationFilter.class)
+            .addFilterBefore(new JwtFilter(),UsernamePasswordAuthenticationFilter.class)
+            .csrf().disable();
+  }
+
+}
