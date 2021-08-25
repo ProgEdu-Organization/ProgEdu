9b009ad35d4d5d133948b746189df4282bc5124a
franky3020 <210238hwsh@gmail.com>
2021-08-26 00:06:00 +0800

    done

--- server_spring/src/main/java/fcu/selab/progedu/WebSecurityConfig.java
+++ server_spring/src/main/java/fcu/selab/progedu/WebSecurityConfig.java
@@ -46,7 +46,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
   public void configure(WebSecurity web) throws Exception {
     web.ignoring().antMatchers("/publicApi/**", "/assignment/getAssignmentFile",
             "/assignment/getMvnAssignmentFile", "/assignment/getJavaAssignmentFile",
-            "/assignment/getAndroidAssignmentFile");
+            "/assignment/getAndroidAssignmentFile", "/assignment/getWebAssignmentFile");
   }
 
 
