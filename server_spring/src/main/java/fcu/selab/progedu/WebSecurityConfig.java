8e4f6ac20ec93136f37b63a30ee187f50d1e1cfc
franky3020 <210238hwsh@gmail.com>
2021-08-25 23:35:08 +0800

    fix

--- server_spring/src/main/java/fcu/selab/progedu/WebSecurityConfig.java
+++ server_spring/src/main/java/fcu/selab/progedu/WebSecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -33,19 +34,23 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     http.cors().configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues());
 
     http.authorizeRequests()
-            .antMatchers(HttpMethod.POST, "/publicApi/**").permitAll()
-            .antMatchers(HttpMethod.GET, "/assignment/getAssignmentFile").permitAll()
-            .antMatchers(HttpMethod.GET, "/assignment/getMvnAssignmentFile").permitAll()
-            .antMatchers(HttpMethod.GET, "/assignment/getJavaAssignmentFile").permitAll()
-            .antMatchers(HttpMethod.GET, "/assignment/getAndroidAssignmentFile").permitAll()
-            .antMatchers(HttpMethod.GET, "/assignment/getWebAssignmentFile").permitAll()
             .antMatchers(HttpMethod.POST, "/login").permitAll()
-
             .anyRequest().authenticated()
             .and()
             .addFilterBefore(new JwtLoginFilter("/login",authenticationManager()), UsernamePasswordAuthenticationFilter.class)
             .addFilterBefore(new JwtFilter(),UsernamePasswordAuthenticationFilter.class)
             .csrf().disable();
   }
+
+  @Override
+  public void configure(WebSecurity web) throws Exception {
+    web.ignoring().antMatchers("/publicApi/**");
+    web.ignoring().antMatchers("/assignment/getAssignmentFile");
+    web.ignoring().antMatchers("/assignment/getMvnAssignmentFile");
+    web.ignoring().antMatchers("/assignment/getJavaAssignmentFile");
+    web.ignoring().antMatchers("/assignment/getAndroidAssignmentFile");
+  }
+
+
 
 }
