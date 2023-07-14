f5daf5ab69564894e7b421c686ee852fe7d2429d
fcumselab <fcumselab@gmail.com>
2023-07-14 22:18:33 +0800

    fix MongoDB Connection

--- server_spring/src/main/java/fcu/selab/progedu/ProgeduApplication.java
+++ server_spring/src/main/java/fcu/selab/progedu/ProgeduApplication.java
@@ -2,8 +2,9 @@ package fcu.selab.progedu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {MongoAutoConfiguration.class, MongoAutoConfiguration.class})
 public class ProgeduApplication {
 
 	public static void main(String[] args) {
